2e0d247b5bbb7be8dd9facdec1c26ef1a917fbe5
Samara Grant <samara.amanda01@gmail.com>
2020-08-04 22:17:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,11 +8,13 @@ class Anagram
     @some_word = some_word
   end
 
+
   def match(array_of_words)
     array_of_words.each do |array|
       array.split("").sort == @some_word.split("").sort
     end
   end
+end
 
 
 
