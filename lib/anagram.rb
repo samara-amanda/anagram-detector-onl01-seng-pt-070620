3284ce22325a03d8a7143a76c05fa6166aab6209
Samara Grant <samara.amanda01@gmail.com>
2020-08-04 22:17:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,9 +4,21 @@ class Anagram
   attr_accessor :some_word
 
 
-  def initialize(anagrams)
-    @anagrams = anagrams
+  def initialize(some_word)
+    @some_word = some_word
   end
+
+  def match(array_of_words)
+    array_of_words.each do |array|
+      array.split("").sort == @some_word.split("").sort
+    end
+  end
+  
+
+
+
+  end
+
 
 
 
