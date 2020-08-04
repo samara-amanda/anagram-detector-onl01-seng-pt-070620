48b51d1b75b4d84b160a8b2e0211bf62194e1fe9
Samara Grant <samara.amanda01@gmail.com>
2020-08-04 23:26:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :some_word
+
+
+  def initialize(some_word)
+    @some_word = some_word
+  end
+
+
+  def match(array_of_words)
+    array_of_words.select do |array|
+      array.split("").sort == @some_word.split("").sort
+    end
+  end
+end
